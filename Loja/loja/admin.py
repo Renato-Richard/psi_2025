595aff6944ec6aa03ce33e6af7ac3c4a2a4fe55e
RENATO RICHARD CÂMARA DA SILVA <20221011110022@ifrn.local>
2025-04-28 15:40:07 -0300

    update

--- Loja/loja/admin.py
+++ Loja/loja/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from django.contrib import admin #isso já vai estar existindo no arquivo
 # Register your models here.
+from .models import * #imporata nossos models
+admin.site.register(Fabricante) #adiciona a interface do adm
+# Register your models here.
+from django.contrib import admin
+# Register your models here.
+from .models import *
+admin.site.register(Fabricante)
+admin.site.register(Categoria)
+admin.site.register(Produto)
