694e93b40011cee3c6a61673a9b3f49a0fb73da2
RENATO RICHARD CÂMARA DA SILVA <20221011110022@ifrn.local>
2025-11-11 15:51:42 -0300

    estudando

--- Loja/loja/admin.py
+++ Loja/loja/admin.py
@@ -6,9 +6,8 @@ class FabricanteAdmin(admin.ModelAdmin):
     date_hierarchy = 'criado_em'
 class ProdutoAdmin(admin.ModelAdmin):
     date_hierarchy = 'criado_em'
-    list_display = ('Produto', 'destaque', 'promocao', 'msgPromocao', 'preco', 'categoria',)
     empty_value_display = 'Vazio'
-    fields = ('Produto', 'destaque', 'promocao', 'msgPromocao', 'preco', 'categoria',)
     search_fields = ('Produto',)
 admin.site.register(Fabricante, FabricanteAdmin)
 admin.site.register(Produto, ProdutoAdmin)
+admin.site.register(Usuario)
